70db534bba3a6fb10344a4a7fa57874b2c2d3923
gustavo123z <159156773+gustavo123z@users.noreply.github.com>
2024-10-08 12:18:01 -0300

    Add files via upload
    08/10/24

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+'programação',
+'o que é javascript?',
+'o javascript é uma linguagem de programação',
+
+)
+
+criaCartao(
+'Geografia',
+'qual a capital da frança?',
+'A capital da frança é paris'
+
+)
+
+criaCartao(
+'programação',
+'o que é uma função',
+'uma função é um blocode códigos que executa uma função'
+
+)
+
+criaCartao(
+'matemática',
+'qual éo resultado de 6 x 8?',
+'o resultado é 48'
+
+)
